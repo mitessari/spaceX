3373f058f1b7f43ed7beaf29b900ac623ce895eb
Milena <mitessari@yahoo.com.br>
2024-03-15 22:56:36 +0000

    added some functionalities to the page for exemple hover effects, hamburguer menu effects, scroll effectsto the sections.

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 let isActive = false;
 function toggle() {
     if(isActive) {
-        document.querySelector('.hamburger').className = 'hamburguer';
+        document.querySelector('.hamburger').className = 'hamburger';
         document.querySelector('.shadow').className = 'shadow';
         document.querySelector('.mobile-nav').className = 'mobile-nav';
         isActive = false;
     } else {
-        document.querySelector('.hamburger').className = 'hamburguer active';
+        document.querySelector('.hamburger').className = 'hamburger active';
         document.querySelector('.shadow').className = 'shadow active';
         document.querySelector('.mobile-nav').className = 'mobile-nav active';
         isActive = true;
     }
 }
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('active');
+        } else {
+            entry.target.classList.remove('active');
+        }
+    });
+});
+
+document.querySelectorAll('section').forEach(section => {
+    observer.observe(section);
+})
+
+
